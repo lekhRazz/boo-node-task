fc7845ca83d39f2d7111e0f0c6ae46a347d90141
lekhrazz <lekh.rokaya@rakutentravelxchange.com>
2024-02-19 18:05:33 +0545

    modified:   test/comment-controller.test.js

--- test/comment-controller.test.js
+++ test/comment-controller.test.js
@@ -3,11 +3,15 @@ import { describe, jest, expect, test } from '@jest/globals';
 import HttpStatus from 'http-status';
 
 import commentController from '../lib/controller/comment-controller';
-import dummyCommentData from "./data/comment.js"
+import CommentService from "../lib/services/comment-service.js";
 
-describe('like comment test', () => {
+// import dummyCommentData from "./data/comment.js"
 
-  test('returns NOT_FOUND (404) when no comment is sent in params response', async () => {
+jest.mock('../lib/services/comment-service.js');
+
+describe('like comment test suite', () => {
+
+  it('returns NOT_FOUND (404) when no comment is sent in params', async () => {
     try {
       let req = {
         params: {commentId: ""}
@@ -27,8 +31,65 @@ describe('like comment test', () => {
         message: "Comment not found."
       })
     } catch (e) {
-      console.log("error", e)
+      console.log("error on NOT_FOUND test", e)
     }
   });
 
+  it('returns NOT_MODIFIED (304) when comment is not saved', async () => {
+    try {
+      let req = {
+        params: {commentId: "123"},
+        dbConnection: function() {}
+      }
+      let res = {
+        status: function () { },
+        json: function () { }
+      }
+      const statusSpied = jest.spyOn(res, "status");
+      const jsonSpied = jest.spyOn(res, "json");
+
+      let next = function () { };
+
+      //@mock likeComment method of CommentService
+      CommentService.likeComment.mockResolvedValue({});
+
+      await commentController.likeComment(req, res, next);
+      expect(statusSpied).toHaveBeenCalledWith(HttpStatus.NOT_MODIFIED);
+      expect(jsonSpied).toHaveBeenCalledWith({
+        status: HttpStatus.NOT_MODIFIED,
+        message: "Comment like failed."
+      })
+    } catch (e) {
+      console.log("error on NOT_MODIFIED test", e)
+    }
+  });
+
+  it('returns OK (200) when comment is saved', async () => {
+    try {
+      let req = {
+        params: {commentId: "123"},
+        dbConnection: function() {}
+      }
+      let res = {
+        status: function () { },
+        json: function () { }
+      }
+      const statusSpied = jest.spyOn(res, "status");
+      const jsonSpied = jest.spyOn(res, "json");
+
+      let next = function () { };
+
+      //@mock likeComment method of CommentService
+      CommentService.likeComment.mockResolvedValue({"key":"value"});
+
+      await commentController.likeComment(req, res, next);
+      expect(statusSpied).toHaveBeenCalledWith(HttpStatus.OK);
+      expect(jsonSpied).toHaveBeenCalledWith({
+        status: HttpStatus.OK,
+        message: "Comment is liked."
+      })
+    } catch (e) {
+      console.log("error on success test", e)
+    }
+  });
 });
